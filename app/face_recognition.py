80cf91be14a95b928d8ea578656f1d169f30a336
Luca Bianchi <bianchi.luca@gmail.com>
2023-03-30 17:45:14 +0200

    :bug: Fix multiple faces

--- app/face_recognition.py
+++ app/face_recognition.py
@@ -19,11 +19,9 @@ class FaceRecognition():
         self.facerec = dlib.face_recognition_model_v1(
             abspath(config.dlib_face_recognition_model_file))
         self.faces = []
-        self.face_names = []
-        self.face_descriptors = []
 
     def encode_faces(self, image):
-        # img = dlib.load_rgb_image(join(faces_path, image))
+        face_encoded = None, None
         if image is None:
             logging.error("Empty image")
             raise Exception("Empty image")
@@ -37,19 +35,22 @@ class FaceRecognition():
             face_aligned = dlib.get_face_chip(image, shape)
             face_descriptor = np.array(self.facerec.compute_face_descriptor(
                 face_aligned))
-            return face_aligned, face_descriptor
+            face_encoded = (face_aligned, face_descriptor)
         else:
             logging.error("No faces found in image")
-            return None, None
+
+        return face_encoded
 
     def load_faces(self):
         # load faces from faces folder
-        for face in os.listdir(faces_path):
-            if not face.startswith("."):
-                logging.info("Loading face: {}".format(face))
-                img = cv2.imread(join(faces_path, face))
-                self.faces.append(self.encode_faces(img))
-                self.face_names.append(face.split(".")[0])
+        for face_file in os.listdir(faces_path):
+            if not face_file.startswith("."):
+                logging.info("Loading face: {}".format(face_file))
+                img = cv2.imread(join(faces_path, face_file))
+                face, desc = self.encode_faces(img)
+                name = face_file.split(".")[0]
+                self.faces.append((face, desc, name))
+
         logging.info("Loaded {} faces".format(len(self.faces)))
 
     def compute_distance(self, face1_representation, face2_representation):
@@ -64,27 +65,42 @@ class FaceRecognition():
 
     def recognize_face(self, image):
         # recognize face in image
+
         (face, descriptor) = self.encode_faces(image)
-        index = -1
-        face_detection = -1
+
+        face_recognized = False
         face_name = None
         if (face is not None):
-            for (face, desc) in self.faces:
+            if (config.save_detected_faces):
+                ts = time()
+                save_path = abspath("./tmp")
+                cv2.imwrite(f"{save_path}/{face_name}-{ts}.png", image)
+
+            minimum_distance = None
+            minimum_distance_index = -1
+            most_similar_face = None
+
+            for (face, desc, name) in self.faces:
+
                 dist = self.compute_distance(desc, descriptor)
-                if dist < 0.6:
-                    logging.info("Face recognized: {}".format(
-                        self.face_names[index]))
-                    face_detection = 1
-                    face_name = self.face_names[index]
-                else:
-                    logging.info("Face not recognized")
-                    face_detection = 0
-                if (config.save_detected_faces):
-                    ts = time()
-                    save_path = abspath("./tmp")
-                    cv2.imwrite(f"{save_path}/{face_name}-{ts}.png", image)
-                index += 1
+                # logging.info(f"Distance for {name}: {dist}")
+                if minimum_distance == None or dist < minimum_distance:
+                    minimum_distance = dist
+                    most_similar_face = (face, desc, name)
+
+            if minimum_distance < 0.559:
+
+                face, desc, name = most_similar_face
+
+                logging.info(f"Face recognized: {name}")
+                face_recognized = True  # detected
+            else:
+                #    logging.info("Face not recognized")
+                face_recognized = False  # undetected
+
+            face, desc, name = most_similar_face
+
+            return (face_recognized, name)
         else:
             logging.info("No faces found")
-            face_detection = -1
-        return (face_detection, face_name)
+            return None, None
--- app/logic.py
+++ app/logic.py
@@ -9,28 +9,31 @@ from observers import Observer
 
 class Logic (Observer):
     def __init__(self):
-        self.recognized_faces = 0
+        self.recognized_faces = []
         self.faces_treshold = config.counter_threshold
         self.bunker = Bunker()
         self.detector = FaceRecognition()
         self.detector.load_faces()
 
     def recognize_face(self, frame):
-        res, name = self.detector.recognize_face(frame)
-        if res == 1:
-            self.recognized_faces += 1
-            if self.recognized_faces >= self.faces_treshold:
-                self.recognized_faces = 0
-                logging.info(
-                    f"Logic recognized face {name}. Triggering action...")
-                self.bunker.activate(name)
-                # sleep(config.sleep_time)
-        elif res == 0:
-            logging.info("Found a face, but not recognized")
-            self.recognized_faces = 0
-            self.bunker.announce_unknown()
+        isFound, face_name = self.detector.recognize_face(frame)
+
+        if isFound is not None:
+            if isFound is True:
+
+                self.recognized_faces.append(face_name)
+                if len(self.recognized_faces) >= self.faces_treshold:
+                    self.recognized_faces = []
+                    logging.info(
+                        f"Logic recognized face {face_name}. Triggering action...")
+                    self.bunker.activate(face_name)
+
+            else:
+                logging.info("Found a face, but not recognized")
+                self.recognized_faces = []
+                self.bunker.announce_unknown()
         else:
-            self.recognized_faces = 0
+            self.recognized_faces = []
             logging.info("Logic: No faces found")
 
     def process_frame(self, frame):
